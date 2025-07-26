367893a504a7f8d583ef56cab6591d7d2b455f7a
AlexMog <alexmog@live.fr>
2025-07-26 13:44:05 +0000

    chore: update versions for react-sdk and typescript-sdk to 2.0.3 and 1.2.3 respectively, and streamline files configuration in package.json

--- packages/typescript-sdk/src/typed-client.ts
+++ packages/typescript-sdk/src/typed-client.ts
@@ -1,4 +1,5 @@
 import { isNil, omitBy } from "lodash";
+import { getToken } from "./auth";
 import { createClient } from "./generated";
 import type { ClientOptions } from "./generated/runtime";
 
@@ -7,21 +8,41 @@ export const createTypedClient = (
 		apiBaseUrl?: string;
 		organizationId: string;
 		token?: string;
+		application?: {
+			clientId: string;
+			clientSecret: string;
+		};
 	} & Omit<ClientOptions, "url" | "headers" | "batch" | "keepalive" | "method">,
-) =>
-	createClient({
-		url: `https://api.warrior.${props.apiBaseUrl ?? "well-played.gg"}/graphql`,
+) => {
+	const url = props.apiBaseUrl
+		? `https://api.warrior.${props.apiBaseUrl}/graphql`
+		: undefined;
+	const oauthUrl = `https://oauth.warrior.${props.apiBaseUrl ?? "well-played.gg"}`;
+	return createClient({
+		url,
 		batch: {
 			batchInterval: 100,
 			maxBatchSize: 10,
 		},
-		headers: omitBy(
-			{
-				"organization-id": props.organizationId,
-				authorization: props.token ? `Bearer ${props.token}` : undefined,
-			},
-			isNil,
-		) as Record<string, string>,
+		headers: async () => {
+			const token =
+				props.token ??
+				(props.application
+					? await getToken(
+							props.application.clientId,
+							props.application.clientSecret,
+							oauthUrl,
+						)
+					: undefined);
+			return omitBy(
+				{
+					"organization-id": props.organizationId,
+					authorization: token ? `Bearer ${token}` : undefined,
+				},
+				isNil,
+			) as Record<string, string>;
+		},
 		keepalive: true,
 		method: "POST",
 	});
+};
